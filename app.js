28fceb9f31c823d577831ba08948f03c45b136d8
Marshal Wu <MarshalW@users.noreply.github.com>
2015-12-17 10:50:28 +0800

    update port to 3000

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ function respond(req, res, next) {
 var server = restify.createServer();
 server.get('/hello/:name', respond);
 
-server.listen(8080, function() {
+server.listen(3000, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
