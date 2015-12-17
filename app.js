8d5db80d25773c58559aa2ec9be8667daea6404d
Marshal Wu <MarshalW@users.noreply.github.com>
2015-12-17 16:44:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -15,5 +15,6 @@ server.listen(3000, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
 
-var redisDatabaseNumber=process.env.REDIS_DATABASE;
-console.log(redisDatabaseNumber);
+if(process.env.REDIS_PORT_6379_TCP_PORT && process.env.REDIS_DATABASE){
+  console.log('redis link ok.');
+}
