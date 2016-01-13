1b239f962d165f4ba318ad16f945031aa1eb31c3
Marshal <marshal.wu@qq.com>
2016-01-13 12:19:52 +0800

    测试异常日志

--- logger.js
+++ logger.js
@@ -5,6 +5,22 @@ const winston=require('winston');
 
 let logger = winston;
 
+// console.log(logger.exceptionHandlers);
+
+// Extend a winston by making it expand errors when passed in as the 
+// second argument (the first argument is the log level).
+function expandErrors(logger) {
+  var oldLogFunc = logger.log;
+  logger.log = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    if (args.length >= 2 && args[1] instanceof Error) {
+      args[1] = args[1].stack;
+    }
+    return oldLogFunc.apply(this, args);
+  };
+  return logger;
+}
+
 // 如果有／log目录，说明在docker环境下
 if (fs.existsSync('/log')){
  //    logger=new (winston.Logger)({
@@ -51,7 +67,9 @@ if (fs.existsSync('/log')){
 	    true
 	);
 
-	// logger.handleExceptions(new winston.transports.File({ filename: '/log/crash.log'}));
+	logger.handleExceptions(new winston.transports.File({ filename: '/log/crash.log'}));
+
+	logger=expandErrors(logger);
 }
 
 module.exports=logger;
