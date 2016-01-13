65df7fe3bbaaca75e6fe3c84321fafee0a93d5d5
Marshal <marshal.wu@qq.com>
2016-01-13 14:56:14 +0800

    加入stack-trace

--- logger.js
+++ logger.js
@@ -3,74 +3,44 @@
 const fs = require('fs');
 const winston=require('winston');
 
+var stackTrace = require('stack-trace');
+
 let logger = winston;
-
-// console.log(logger.exceptionHandlers);
-
-// Extend a winston by making it expand errors when passed in as the 
-// second argument (the first argument is the log level).
-function expandErrors(logger) {
-  var oldLogFunc = logger.log;
-  logger.log = function() {
-    var args = Array.prototype.slice.call(arguments, 0);
-    if (args.length >= 2 && args[1] instanceof Error) {
-      args[1] = args[1].stack;
-    }
-    return oldLogFunc.apply(this, args);
-  };
-  return logger;
-}
 
 // 如果有／log目录，说明在docker环境下
 if (fs.existsSync('/log')){
- //    logger=new (winston.Logger)({
-	//   transports: [
-	//     new (winston.transports.File)({
-	//       name: 'all',
-	//       filename: '/log/all.log',
-	//       level: 'info'
-	//     }),
-	//     new (winston.transports.File)({
-	//       name: 'error',
-	//       filename: '/log/error.log',
-	//       level: 'error'
-	//     })
-	//   ]
-	// });
-	// winston.add(winston.transports.File, {
-	//     filename: '/log/all.log',
-	//     level: 'info'
-	// });
-
-	// winston.add(winston.transports.File, {
-	//     filename: '/log/error.log',
-	//     level: 'error'
-	// });
-
-	winston.add(
+    logger=new (winston.Logger)({
+	  transports: [
 	    new (winston.transports.File)({
 	      name: 'all',
 	      filename: '/log/all.log',
 	      level: 'info'
 	    }),
-	    {},
-	    true
-	);
-
-	winston.add(
 	    new (winston.transports.File)({
 	      name: 'error',
 	      filename: '/log/error.log',
 	      level: 'error'
-	    }),
-	    {},
-	    true
-	);
+	    })
+	  ]
+	});
 
-	logger.handleExceptions(new winston.transports.File({ filename: '/log/crash.log'}));
+	// logger.handleExceptions(new winston.transports.File({ filename: '/log/crash.log'}));
 
-	// logger=expandErrors(logger);
+	// logger.on('error', function (err) { 
+	// 	console.log('发生了没有捕获的异常！');
+	// });
+
+	logger.on('logging', function (transport, level, msg, meta) {
+		// if(level==='error'){
+		// 	meta.error={
+		// 		fileName:
+		// 	}
+		// }
+		// var trace = stackTrace.get();
+		var trace = stackTrace.get();
+		meta.fileName=trace[1].getFileName();
+		meta.lineNumber=trace[1].getLineNumber();
+	});
 }
 
-module.exports=expandErrors(logger);
-// module.exports=logger;
+module.exports=logger;
